4b2b557c7a7b6f13619d3c61d3d1121fff0bda95
Danny Gitelman <55748709+gitelmandanny@users.noreply.github.com>
2021-02-23 13:32:41 +0200

    Update CurrencyRouletteGame.py

--- WorldOfGames/CurrencyRouletteGame.py
+++ WorldOfGames/CurrencyRouletteGame.py
@@ -22,7 +22,7 @@ class CurrencyRouletteGame(ABC):
 
     @staticmethod
     def convert_usd_to_ils():
-        url = 'https://free.currconv.com/api/v7/convert?q=USD_ILS&compact=ultra&apiKey=745a7a1c61a087e2ed35'
+        url = 'https://free.currconv.com/api/v7/convert?q=USD_ILS&compact=ultra&apiKey=745a7a1c61a087e2ed35' # if you up for another challenge, you can think gow to pass the secret key securly and not in raw code
         res = r = requests.get(url)
         if res.status_code == 200:
             return res.json()['USD_ILS']
